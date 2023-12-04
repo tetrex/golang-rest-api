aef6a3571a366c49f0ba16a35ea69c9fe82243f2
harshR <42332761+tetrex@users.noreply.github.com>
2023-12-04 13:20:50 +0530

    feat : added server mux

--- main.go
+++ main.go
@@ -3,11 +3,22 @@ package main
 import (
 	"io"
 	"net/http"
+	"time"
 )
 
 func main() {
-	http.HandleFunc("/hello", hello)
-	http.ListenAndServe(":8080", nil)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/hello", hello)
+
+	s := &http.Server{
+		Addr:         ":8080",
+		Handler:      mux,
+		ReadTimeout:  2 * time.Second,
+		WriteTimeout: 2 * time.Second,
+		IdleTimeout:  5 * time.Second,
+	}
+
+	s.ListenAndServe()
 }
 
 func hello(w http.ResponseWriter, req *http.Request) {
